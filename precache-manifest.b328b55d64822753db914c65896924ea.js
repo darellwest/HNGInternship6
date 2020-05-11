13ccb160d5bf851d786b2dddb7089442cd84e6bd
darellwest <ewere.njoagwu@gmail.com>
2020-05-11 05:13:55 +0100

    Updates

---
+++ precache-manifest.b328b55d64822753db914c65896924ea.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9bca4a60f1f4211557bd083fc78a6220",
+    "url": "/index.html"
+  },
+  {
+    "revision": "02f550c880396f4e5a5f",
+    "url": "/static/css/main.b0371623.chunk.css"
+  },
+  {
+    "revision": "819dd0aed6dfd694d5e4",
+    "url": "/static/js/2.cc3d7fa9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.cc3d7fa9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "02f550c880396f4e5a5f",
+    "url": "/static/js/main.6b2cfb5f.chunk.js"
+  },
+  {
+    "revision": "96e8b1c6ec2ba9aad501",
+    "url": "/static/js/runtime-main.6ce7f017.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
